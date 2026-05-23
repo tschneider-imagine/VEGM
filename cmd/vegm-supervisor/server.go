9ee14ba66bc83f9272f04cc3740cff77c4234229
tschneider-imagine <tschneider@imagineemail.com>
2026-05-23 12:05:50 -0600

    Register fleet instance add route

--- cmd/vegm-supervisor/server.go
+++ cmd/vegm-supervisor/server.go
@@ -85,6 +85,7 @@ func (s *supervisorServer) routes() http.Handler {
 	mux.HandleFunc("/api/instances/stop-all", s.handleStopAll)
 	mux.HandleFunc("/api/instances/", s.handleInstanceAction)
 	mux.HandleFunc("/api/instance-settings/", s.handleInstanceSettings)
+	mux.HandleFunc("/api/fleet/instances/add", s.handleFleetInstancesAdd)
 	mux.HandleFunc("/api/export/fleet", s.handleFleetExport)
 	return withSupervisorCORS(mux)
 }
